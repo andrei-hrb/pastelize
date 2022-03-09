2cb0366e683b05c6ed8cd5b2844e1bb427caa76c
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-03-09 16:38:12 +0200

    added input fields

--- src/Main.js
+++ src/Main.js
@@ -9,6 +9,12 @@ const Main = ({ color, pastel, handleColorChange }) => (
                     className='absolute -inset-2 w-44 h-44'
                 />
             </div>
+            <input
+                type='text'
+                className='text-xl border text-center w-40 rounded-lg'
+                value={color}
+                onChange={handleColorChange}
+            />
             <span>⬇️</span>
             <div className='relative rounded-lg w-40 h-40 overflow-hidden'>
                 <input
@@ -18,6 +24,12 @@ const Main = ({ color, pastel, handleColorChange }) => (
                     readOnly
                 />
             </div>
+            <input
+                type='text'
+                className='text-xl border text-center w-40 rounded-lg'
+                value={pastel}
+                readOnly
+            />
         </div>
     </main>
 )
