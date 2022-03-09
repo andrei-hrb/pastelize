51247e9672e90f2f044e5c74ca41a1eb93bb4fa8
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-03-09 16:15:38 +0200

    added github repo

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,8 @@ const Footer = () => (
     <footer className='text-xs self-end'>
         <code>
             Crafted in 2022 by <a href='https://hirbu.ro/'>Hîrbu Andrei</a>,
-            thanks to React + Tailwind + TinyColor. Check this project's Github
-            repo.
+            thanks to React + Tailwind + TinyColor. Check this project's{' '}
+            <a href='https://github.com/andrei-hrb/pastelize'>Github repo.</a>
         </code>
     </footer>
 )
