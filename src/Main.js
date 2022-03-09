caeeea568306e8365362d5f230efa00163c8a607
Hîrbu Andrei <andrei_hrb@icloud.com>
2022-03-09 16:23:37 +0200

    making the squares smaller

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,19 @@
 const Main = ({ color, pastel, handleColorChange }) => (
     <main>
         <div className='flex flex-col h-full justify-between items-center'>
-            <div className='relative rounded-lg w-72 h-72 overflow-hidden'>
+            <div className='relative rounded-lg w-40 h-40 overflow-hidden'>
                 <input
                     type='color'
                     value={color}
                     onChange={handleColorChange}
-                    className='absolute -inset-2 w-80 h-80'
+                    className='absolute -inset-2 w-44 h-44'
                 />
             </div>
             <span>⬇️</span>
-            <div className='relative rounded-lg w-72 h-72 overflow-hidden'>
+            <div className='relative rounded-lg w-40 h-40 overflow-hidden'>
                 <input
                     type='color'
-                    className='absolute -inset-2 w-80 h-80'
+                    className='absolute -inset-2 w-44 h-84'
                     value={pastel}
                     readOnly
                 />
